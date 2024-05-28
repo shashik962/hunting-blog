731445b8c4e1a64d9dbd049fd8633a4669c18e2e
shashi_kumar <shashi.kumar@gingerwebs.co.in>
2024-05-28 17:21:12 +0530

    Component Level CSS

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Script from 'next/script'
 import styles from '../styles/Home.module.css'
+// import styles1 from '../styles/Home1.module.css'
+// import styles2 from '../styles/Home2.module.css'
 import Link from 'next/link'
 
 export default function Home() {
@@ -27,38 +29,24 @@ export default function Home() {
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          A bog for hunting coders by hunting coder
         </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        {/* <div className={styles1.con}> */}
+        {/* <div className={`${styles1.con} ${styles2.con}`}> */}
+        <div >
+            <h2>Popular Blog</h2>
+            <div className='blogItem'>
+              <h3>How to learn Javascript in 2022?</h3>
+              <p>Javascript is the language used to design logic for the web</p>
+            </div>
+            <div className='blogItem'>
+              <h3>How to learn Javascript in 2022?</h3>
+              <p>Javascript is the language used to design logic for the web</p>
+            </div>
+            <div className='blogItem'>
+              <h3>How to learn Javascript in 2022?</h3>
+              <p>Javascript is the language used to design logic for the web</p>
+            </div>
         </div>
       </main>
 

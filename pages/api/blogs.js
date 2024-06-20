306b70541b149b3617b56204007ef44e9310d82c
shashi_kumar <shashi.kumar@gingerwebs.co.in>
2024-06-20 12:30:02 +0530

    Using the API Endpoints | NextJs Tutorial for Beginners #19

---
+++ pages/api/blogs.js
@@ -0,0 +1,26 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import * as fs from 'fs';
+
+export default async function handler(req, res) {
+  let data = await fs.promises.readdir('blogdata');
+  let myfile;
+  let allBlogs = [];
+  for(let i = 0; i < data.length; i++) {
+    const item = data[i];
+    myfile = await fs.promises.readFile(('blogdata/'+item), 'utf-8');
+    console.log(myfile);
+    allBlogs.push(JSON.parse(myfile));
+  }
+  res.status(200).json(allBlogs);
+  // fs.promises.readdir("blogdata", (err, data) => {
+  //   console.log(data);
+  //   let allBlogs = [];
+  //   data.forEach(item => {
+  //     fs.readFile(('blogdata/'+ item), (d) => {
+  //       allBlogs.push(d);
+  //     })
+  //   });
+  //   res.status(200).json(allBlogs)
+  // })
+  
+}
--- pages/blog.js
+++ pages/blog.js
@@ -1,15 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from "../styles/Blog.module.css"
 import Link  from 'next/link';
 
 // Step 1: Collect all the files from blogdata directory
 // Step 2: Iterate through the and Display them
 function Blog() {
+  const [blogs, setBlogs] = useState([]);
+  useEffect(() => {
+      console.log('useEffect is running!');
+      fetch('http://localhost:3001/api/blogs').then((a) => {
+        return a.json();
+      }).then((parsed) => {
+          console.log(parsed);
+          setBlogs(parsed);
+      });
+  }, [])
   return (
     <div className={styles.container}>
         <main className={styles.main} >
-              <h2>Latest Blog</h2>
-              <div className={styles.blogItemh1}>
+        <h2>Latest Blog</h2>
+            {blogs.map((blogitem) => {
+              return (
+                <div  key={blogitem.title}>
+                  <Link href={`/blogpost/${blogitem.slug}`} >
+                    <h3 className={styles.blogItemh3}>{blogitem.title}</h3>
+                    <p className={styles.blogItemp}>{blogitem.content.substr(0,400)}</p>
+                  </Link>
+                </div>
+              )
+            })}
+              
+              {/* <div className={styles.blogItemh1}>
                 <Link href={'/blogpost/learn-javascript'} >
                   <h3>How to learn Javascript in 2022?</h3>
                   <p>Javascript is the language used to design logic for the web</p>
@@ -22,7 +43,7 @@ function Blog() {
               <div className='blogItem'>
                 <h3>How to learn Javascript in 2022?</h3>
                 <p>Javascript is the language used to design logic for the web</p>
-              </div>
+              </div> */}
           </main>
     </div>
     
